f543633b803c7bf73ffb22f541758ed7a5a0ed49
sar0j <xyzpro2018@gmail.com>
2026-04-06 19:09:09 +1000

    Small Update - GitOpstest

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def get_db():
 def home():
     env = os.environ.get("ENV", "unknown")
     app_name = os.environ.get("APP_NAME", "unknown")
-    return f"App: {app_name} | ENV: {env} | GitOps with ArgoCD🚀"
+    return f"App: {app_name} | ENV: {env} | GitOps with ArgoCD🚀🚀"
 
 @app.route("/users")
 def get_users():
